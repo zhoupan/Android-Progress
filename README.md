a41bc09d90d417b002905333476d80ff24fdc0dc
Marco <marco1@live.de>
2014-03-14 02:21:10 +0100

    Project setup

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-Android-Progress
-================
+# Android-Progress
 
 Simple progress bars and progress dialogs for Android
+
+## Installation
+
+ * Copy the Java package to your project's source folder
+ * or
+ * Create a new library project from this repository and reference it in your project
+
+## Usage
+
+### SimpleProgressDialog
+
+```
+SimpleProgressDialog dialog;
+dialog = SimpleProgressDialog.show(this);
+// do some work
+dialog.dismiss();
+```
+
+## License
+
+```
+Copyright 2014 www.delight.im <info@delight.im>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
---
+++ src/im/delight/android/progress/SimpleProgressDialog.java
@@ -0,0 +1,43 @@
+package im.delight.android.progress;
+
+/**
+ * Copyright 2014 www.delight.im <info@delight.im>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ProgressBar;
+
+public class SimpleProgressDialog extends Dialog {
+
+	public static SimpleProgressDialog show(Context context) {
+		SimpleProgressDialog dialog = new SimpleProgressDialog(context);
+		
+		dialog.setTitle(null);
+		dialog.setCancelable(false);
+		dialog.setOnCancelListener(null);
+
+		dialog.addContentView(new ProgressBar(context), new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		dialog.show();
+		
+		return dialog;
+	}
+
+	public SimpleProgressDialog(Context context) {
+		super(context, R.style.SimpleProgressDialog);
+	}
+
+}
